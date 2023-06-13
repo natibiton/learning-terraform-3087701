0a517743954753dc8a9dda5927624dd37ec087ee
Nati Biton <natibiton@gmail.com>
2023-06-13 15:57:42 +0300

    Use the VPC aws module

--- main.tf
+++ main.tf
@@ -14,8 +14,25 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{  # Defining a data type to get the default vpc configuration, we will use it later on
-  default = true
+# data "aws_vpc" "default"{  # Defining a data type to get the default vpc configuration, we will use it later on
+#   default = true
+# }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -34,7 +51,8 @@ module "blog_sg" { # From the registery of Terraform
   version = "5.1.0"
   name = "blog"
 
-  vpc_id = data.aws_vpc.default.id  # Reference to the above definition of the aws_vpc
+  #vpc_id = data.aws_vpc.default.id  # Reference to the above definition of the aws_vpc
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
