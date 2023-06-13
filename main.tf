08e437460127150857d2ea185a316623d79963ad
Nati Biton <natibiton@gmail.com>
2023-06-13 16:11:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,8 +24,11 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+#   azs             = ["us-west-2a", "us-west-2b"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  
+  azs             = ["us-west-2a"]
+  public_subnets  = ["10.0.101.0/24"]
 
   tags = {
     Terraform = "true"
