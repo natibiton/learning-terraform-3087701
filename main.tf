988d6454ccc5cbcd1adda8181d9f60f1ec876ddf
Nati Biton <natibiton@gmail.com>
2023-06-14 08:28:25 +0300

    Changes by the 04_02 branch

--- main.tf
+++ main.tf
@@ -14,21 +14,16 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-# data "aws_vpc" "default"{  # Defining a data type to get the default vpc configuration, we will use it later on
-#   default = true
-# }
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-#   azs             = ["us-west-2a", "us-west-2b"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   
-  azs             = ["us-west-2a"]
-  public_subnets  = ["10.0.101.0/24"]
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -36,33 +31,21 @@ module "blog_vpc" {
   }
 }
 
-#resource "aws_instance" "blog" {
-#  ami           = data.aws_ami.app_ami.id
-#  instance_type = var.instance_type
-#  
-#  subnet_id = module.blog_vpc.public_subnets[0]
-#  vpc_security_group_ids = [module.blog_sg.security_group_id]
-#
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
-
 #The autoscalling is replacing the resource of the aws_instance
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.10.0"
   
   name = "blog"
+
   min_size = 1
   max_size = 2
-  
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -99,57 +82,15 @@ module "blog_alb" {
   }
 }
 
-module "blog_sg" { # From the registery of Terraform
+module "blog_sg" { 
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog"
-
-  #vpc_id = data.aws_vpc.default.id  # Reference to the above definition of the aws_vpc
-  #vpc_id = module.vpc.public_subnets[0]
-  vpc_id = module.blog_vpc.default_vpc_id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-# Below is the regular definition, currently not used
-
-# resource "aws_security_group" "blog" {
-#   name        = "blog"
-#   description = "Allow http and https in, allow everything out"
-  
-#   vpc_id = data.aws_vpc.default.id  # Reference to the above definition of the aws_vpc
-# }
-
-# resource "aws_security_group_rule" "blog_http_in" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
-
-# resource "aws_security_group_rule" "blog_https_in" {
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
-
-# resource "aws_security_group_rule" "blog_everything_out" {
-#   type              = "egress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
 
